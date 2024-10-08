68968cc6b4dbf677f57e8562650177a6e022fb79
Hamza <72913359+Hamza12700@users.noreply.github.com>
2024-10-08 15:51:34 +0000

    feat: storing the config options in a struct

--- src/main.zig
+++ src/main.zig
@@ -4,9 +4,25 @@ const fs = std.fs;
 const posix = std.posix;
 const io = std.io;
 const mem = std.mem;
+const fmt = std.fmt;
 
 const BAT_CAPASCITY = "/sys/class/power_supply/BAT0/capacity";
 const BAT_STATUS = "/sys/class/power_supply/BAT0/status";
+
+const Config = struct {
+  ticker: u16,
+  half: u8,
+  mid: u8,
+  low: u8,
+
+  /// Set fields to default options
+  fn defualt(self: *Config) void {
+    self.half = 50;
+    self.mid = 25;
+    self.low = 10;
+    self.ticker = 10;
+  }
+};
 
 pub fn main() !void {
   const stdout = io.getStdOut().writer();
@@ -15,6 +31,9 @@ pub fn main() !void {
     posix.exit(1);
   };
 
+  var capascity_buf: [4]u8 = undefined;
+  var status_buf: [12]u8 = undefined;
+
   var args = std.process.args();
   if (args.inner.count >= 2) {
     // Skip the binary path
@@ -22,35 +41,8 @@ pub fn main() !void {
 
     while (args.next()) |arg| {
       if (mem.eql(u8, arg, "s") or mem.eql(u8, arg, "status")) {
-        const battery_cap_file = fs.openFileAbsolute(BAT_CAPASCITY, .{}) catch |err| {
-          if (err == error.FileNotFound) {
-            std.debug.print("Failed to open battery capascity file: {s}\n", .{BAT_CAPASCITY});
-            std.debug.print("Error: {any}\n", .{err});
-            posix.exit(1);
-          } else {
-            std.debug.print("Unexpected error occur when reading the battery capascity file: {s}\n", .{BAT_CAPASCITY});
-            std.debug.print("Error: {any}\n", .{err});
-            posix.exit(1);
-          }
-        };
-
-        const battery_status_file = fs.openFileAbsolute(BAT_STATUS, .{}) catch |err| {
-          if (err == error.FileNotFound) {
-            std.debug.print("Failed to open battery capascity file: {s}\n", .{BAT_STATUS});
-            std.debug.print("Error: {any}\n", .{err});
-            posix.exit(1);
-          } else {
-            std.debug.print("Unexpected error occur when reading the battery capascity file: {s}\n", .{BAT_STATUS});
-            std.debug.print("Error: {any}\n", .{err});
-            posix.exit(1);
-          }
-        };
-
-        var capascity_buf: [4]u8 = undefined;
-        var status_buf: [12]u8 = undefined;
-
-        _ = try battery_cap_file.read(&capascity_buf);
-        _ = try battery_status_file.read(&status_buf);
+        _ = try readFileAbsolute(BAT_CAPASCITY).read(&capascity_buf);
+        _ = try readFileAbsolute(BAT_STATUS).read(&status_buf);
 
         try stdout.print("Battery Capascity: {s}", .{capascity_buf});
         try stdout.print("Battery Status: {s}", .{status_buf});
@@ -68,10 +60,14 @@ pub fn main() !void {
   defer arena_alloc.deinit();
   const alloc = arena_alloc.allocator();
 
-  const config_path = try std.fmt.allocPrint(alloc, "{s}/batmon.ini", .{home_env});
+  var config: ?Config = undefined;
+
+  const config_path = try fmt.allocPrint(alloc, "{s}/batmon.ini", .{home_env});
   const config_file = fs.openFileAbsolute(config_path, .{ .mode = .read_write }) catch |err| switch (err) {
     error.FileNotFound => blk: {
       try stdout.print("Creating config file at: %s\n", .{});
+      config.?.defualt();
+
       break :blk try writeConfig(config_path);
     },
     else => {
@@ -80,7 +76,41 @@ pub fn main() !void {
       posix.exit(1);
     }
   };
-  defer config_file.close();
+
+  if (config == null) {
+    // TODO: parse the config file
+    @panic("parse the config file");
+  }
+  config_file.close();
+
+  while (true) {
+    _ = try readFileAbsolute(BAT_CAPASCITY).read(&capascity_buf);
+    _ = try readFileAbsolute(BAT_STATUS).read(&status_buf);
+    const trim_capascity_buf = mem.trimRight(u8, &capascity_buf, "\n");
+
+    const capacity = fmt.parseUnsigned(u8, trim_capascity_buf, 10) catch |err| {
+      std.debug.print("Error parsing battery capascity value to unsigned int: {s}\n", .{capascity_buf});
+      std.debug.print("{any}\n", .{err});
+      posix.exit(1);
+    };
+
+    try stdout.print("{d}\n", .{capacity});
+    std.time.sleep(10000);
+  }
+}
+
+fn readFileAbsolute(path: []const u8) fs.File {
+  return fs.openFileAbsolute(path, .{}) catch |err| {
+    if (err == error.FileNotFound) {
+      std.debug.print("File not found: {s}\n", .{path});
+      std.debug.print("Error: {any}\n", .{err});
+      posix.exit(1);
+    } else {
+      std.debug.print("Failed to read file: {s}\n", .{path});
+      std.debug.print("Error: {any}\n", .{err});
+      posix.exit(1);
+    }
+  };
 }
 
 /// Write default config options to file
